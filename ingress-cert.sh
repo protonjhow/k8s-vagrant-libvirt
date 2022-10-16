366689847eb148f19e5358273bed2266643d107d
John Howard <john.howard@proton.ch>
2022-10-16 23:45:39 +0200

    forgot the kubeconfig env again

--- ingress-cert.sh
+++ ingress-cert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-kubectl -n ingress-controller create secret tls default-wildcard --key builtin-ca/out/_.k8s.vagrant.local.key --cert builtin-ca/out/_.k8s.vagrant.local.crt
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl -n ingress-controller create secret tls default-wildcard --key builtin-ca/out/_.k8s.vagrant.local.key --cert builtin-ca/out/_.k8s.vagrant.local.crt
 
 cat <<EOF >ingress-cm-patch.yaml
 data: 
   ssl-certificate: ingress-controller/default-wildcard
 EOF
-kubectl -n ingress-controller patch cm haproxy-ingress --patch-file ingress-cm-patch.yaml
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl -n ingress-controller patch cm haproxy-ingress --patch-file ingress-cm-patch.yaml
 
